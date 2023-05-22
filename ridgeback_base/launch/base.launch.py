76c74372c344f6957b1307a761096d84250e0233
LCBW <lcbw99@gmail.com>
2023-05-22 13:55:23 -0500

    something is broken..

--- ridgeback_base/launch/base.launch.py
+++ ridgeback_base/launch/base.launch.py
@@ -15,24 +15,61 @@
 import os
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from nav2_common.launch import RewrittenYaml
 from ament_index_python.packages import get_package_share_directory
 
 
-def generate_launch_description():
+def generate_launch_description(): 
+    physical_robot = LaunchConfiguration('physical_robot')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    control_yaml_file = LaunchConfiguration('control_yaml_file')
+    config = LaunchConfiguration('config')
+
+    declare_physical_robot_cmd = DeclareLaunchArgument(
+        'physical_robot',
+        default_value='true',
+        description='Physical robot if true, simulation if false')
+
+    declare_control_yaml_cmd = DeclareLaunchArgument(
+        'control_yaml_file',
+        default_value=get_package_share_directory('ridgeback_control')+'/config/control.yaml',
+        description='The config file for gazebo_ros2_control, only needed in simulation')
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Whether or not to use simulation time')
+
     description_path = os.path.join(get_package_share_directory(
         'ridgeback_description'), 'launch', 'description.launch.py')
+
+    declare_config_cmd = DeclareLaunchArgument(
+        'config',
+        default_value=os.getenv('RIDGEBACK_CONFIG', 'base'),
+        description='get the ridgeback configuration')
 
     # Specify the actions
     description_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(description_path),
+        launch_arguments={
+            'config': config,
+            'physical_robot': physical_robot,
+            'control_yaml_file': control_yaml_file}.items()
     )
 
-    start_ridgeback_control = IncludeLaunchDescription(
+    start_ridgeback_control = TimerAction(
+            period=20.0,
+            actions=[IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(
             get_package_share_directory('ridgeback_control'), 'launch', 'control.launch.py')),
-    )
+            launch_arguments={
+                'physical_robot': physical_robot,
+                'control_yaml_file': control_yaml_file}.items())
+                ]
+        )
 
     start_teleop_control = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(
@@ -40,6 +77,11 @@ def generate_launch_description():
     )
 
     ld = LaunchDescription()
+    ld.add_action(declare_physical_robot_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_control_yaml_cmd)
+    ld.add_action(declare_config_cmd)
+
     ld.add_action(description_cmd)
     ld.add_action(start_ridgeback_control)
     ld.add_action(start_teleop_control)
